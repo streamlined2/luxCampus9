f80fcbecbc57ecbeddba5a2e0bc61c148fa15bc3
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2021-10-31 15:55:42 +0200

    First draft
    1. methods skeletons defined.

---
+++ luxCampus9/src/main/java/org/training/campus/inspector/Inspector.java
@@ -0,0 +1,36 @@
+package org.training.campus.inspector;
+
+public final class Inspector {
+	
+	private Inspector() {}
+	
+	public static <T> T spawnObject(Class<T> cl){
+		return null;//
+	}
+	
+	public static <T> void callMethodsWithNoArgs(T obj) {
+		//TODO
+	}
+	
+	public static <T> void printMethodSignaturesWithFinal(T obj) {
+		//TODO
+	}
+	
+	public static <T> void printNonPublicMethods(Class<T> cl) {
+		//TODO
+	}
+	
+	public static <T> void printAncestorsAndImplementedInterfaces(Class<T> cl) {
+		//TODO
+	}
+	
+	public static <T> void initializePrivateFields(T obj) {
+		//TODO
+	}	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

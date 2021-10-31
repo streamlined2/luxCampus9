3e94dd6c4d64297d22f594b9f8803187f2fc2952
Serhii Pylypenko <serhii.pylypenko.0@gmail.com>
2021-10-31 16:53:11 +0200

    Third draft
    1. method 'callMethodsWithNoArgs' implemented.

--- luxCampus9/src/main/java/org/training/campus/inspector/Inspector.java
+++ luxCampus9/src/main/java/org/training/campus/inspector/Inspector.java
@@ -1,8 +1,13 @@
 package org.training.campus.inspector;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
+import java.util.Arrays;
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
 
 public final class Inspector {
 
@@ -15,7 +20,19 @@ public final class Inspector {
 	}
 
 	public static <T> void callMethodsWithNoArgs(T obj) {
-		// TODO
+		selectPerform(obj, method -> method.getParameterCount() == 0, (T o, Method m) -> {
+			try {
+				m.setAccessible(true);
+				m.invoke(o);
+			} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private static <T> void selectPerform(T obj, Predicate<Method> methodCheck, BiConsumer<T, Method> action) {
+		Arrays.asList(obj.getClass().getDeclaredMethods()).stream().filter(methodCheck)
+				.forEach(method -> action.accept(obj, method));
 	}
 
 	public static <T> void printMethodSignaturesWithFinal(T obj) {
@@ -42,9 +59,37 @@ public final class Inspector {
 					new Class[] { char[].class, int.class, int.class, MathContext.class },
 					new Object[] { "12345".toCharArray(), 0, 5, MathContext.DECIMAL32 });
 			System.out.printf("created object of BigDecimal: value = %s%n", decimal.toString());
+
+			callMethodsWithNoArgs(new TestClass());
 		} catch (ReflectiveOperationException e) {
 			e.printStackTrace();
 		}
 	}
 
+}
+
+class TestClass {
+	public void a() {
+		System.out.println("a");
+	}
+
+	void b() {
+		System.out.println("b");
+	}
+
+	protected void c() {
+		System.out.println("c");
+	}
+
+	private void d() {
+		System.out.println("d");
+	}
+
+	public void e(int a) {
+		System.out.println("e");
+	}
+
+	void f(String a) {
+		System.out.println("f");
+	}
 }
